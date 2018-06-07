b0be5eda19335cdacce3eed28992da798b6f47ef
Fmressel <franzmichael@resselhaus.com>
2018-06-07 01:02:02 -0700

    feat: Basic offline support using SW and cache

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_ID = 'cache-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_ID).then(cache => {
+    return cache.addAll(['/']);
+  }));
+  console.log('Service Worker installed!');
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request,
+        url = new URL(request.url);
+
+  console.log(url.origin, location.origin);
+  if(url.origin !== location.origin) return;
+
+  event.respondWith(
+    caches.match(url).then(res => {
+      if (res) return res;
+
+      return caches.open(CACHE_ID).then(cache => {
+        return fetch(request).then(fetchRes => {
+          cache.put(url, fetchRes.clone());
+          return fetchRes;
+        });
+      });
+    })
+  );
+});
