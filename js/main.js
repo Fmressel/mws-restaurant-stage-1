c175b2631c72c3e6ccc3daae1b37147b0caf1f74
Fmressel <franzmichael@resselhaus.com>
2018-08-28 11:11:41 -0700

    feat: Add aria-labels to view detail links

--- js/main.js
+++ js/main.js
@@ -175,6 +175,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.setAttribute('role', 'button');
+  more.setAttribute('aria-label', `More information on restaurant ${restaurant.name}`);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more);
 
